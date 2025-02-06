e08dd67f29df81ffcbe1f6c37d7f0e0c30633f10
itsobi <obiobialo@aol.com>
2025-02-05 22:09:57 -0600

    update prod convex url

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,7 +4,11 @@ import { ConvexAuthNextjsProvider } from '@convex-dev/auth/nextjs';
 import { ConvexReactClient } from 'convex/react';
 import { ReactNode } from 'react';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(
+  process.env.NODE_ENV === 'development'
+    ? process.env.DEV_NEXT_PUBLIC_CONVEX_URL!
+    : process.env.NEXT_PUBLIC_CONVEX_URL!
+);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
--- lib/convexClient.ts
+++ lib/convexClient.ts
@@ -1,5 +1,9 @@
 import { ConvexHttpClient } from 'convex/browser';
 
 export const getConvexClient = () => {
-  return new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  return new ConvexHttpClient(
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEV_NEXT_PUBLIC_CONVEX_URL!
+      : process.env.NEXT_PUBLIC_CONVEX_URL!
+  );
 };
