14eabf7f1d48502a9a948a949c0c919311058e23
itsobi <obiobialo@aol.com>
2025-02-10 20:11:42 -0600

    error page in dashboard route

---
+++ app/(app)/dashboard/error.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl font-bold">Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => window.location.reload()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
